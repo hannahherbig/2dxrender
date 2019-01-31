aa9116b9c612d76542acd806cd00df74353ae793
windyfairy <windyfairy@users.noreply.github.com>
2019-01-31 14:29:34 +0900

    Updated code

--- 2dxrender/Program.cs
+++ 2dxrender/Program.cs
@@ -13,6 +13,8 @@ using System.IO;
 
 using CommandLine;
 
+using Newtonsoft.Json;
+
 namespace _2dxrender
 {
     class Program
@@ -56,6 +58,7 @@ namespace _2dxrender
 
             var fileCount = reader.ReadUInt32();
 
+            var tempPath = Path.GetTempPath();
             for (var i = 0; i < fileCount; i++)
             {
                 reader.BaseStream.Seek(i * 8 + 8, SeekOrigin.Begin);
@@ -77,7 +80,7 @@ namespace _2dxrender
                 reader.BaseStream.Seek(offset + dataOffset, SeekOrigin.Begin);
 
                 var audioBytes = reader.ReadBytes(dataSize);
-                var tempFilename = Path.GetTempFileName();
+                var tempFilename = Path.Combine(tempPath, String.Format("{0:d4}.wma", i + 1));
 
                 samples.Add(tempFilename);
 
@@ -92,11 +95,12 @@ namespace _2dxrender
             var samples = new List<string>();
 
             reader.BaseStream.Seek(0x14, SeekOrigin.Begin);
-            
+
             var fileCount = reader.ReadUInt32();
-            
+
             reader.BaseStream.Seek(0x48, SeekOrigin.Begin);
 
+            var tempPath = Path.GetTempPath();
             for (var i = 0; i < fileCount; i++)
             {
                 reader.BaseStream.Seek(0x48 + i * 4, SeekOrigin.Begin);
@@ -117,7 +121,7 @@ namespace _2dxrender
                 reader.BaseStream.Seek(offset + dataOffset, SeekOrigin.Begin);
 
                 var audioBytes = reader.ReadBytes(dataSize);
-                var tempFilename = Path.GetTempFileName();
+                var tempFilename = Path.Combine(tempPath, String.Format("{0:d4}.wav", i + 1));
 
                 samples.Add(tempFilename);
 
@@ -159,7 +163,7 @@ namespace _2dxrender
                     }
                 }
             }
-            
+
             if (options.AssistClap)
             {
                 if (File.Exists(options.AssistClapSound))
@@ -168,7 +172,8 @@ namespace _2dxrender
                     assistClapIdx = samples.Count;
                     samples.Add(assistClapTempFilename);
                     File.Copy(options.AssistClapSound, assistClapTempFilename);
-                } else
+                }
+                else
                 {
                     Console.WriteLine("Couldn't find clap file \"{0}\", skipping...", options.AssistClapSound);
                     options.AssistClap = false;
@@ -176,6 +181,108 @@ namespace _2dxrender
             }
 
             return samples;
+        }
+
+        private static int findSampleById(List<string> samples, int value, string prefix)
+        {
+            for (int idx = 0; idx < samples.Count; idx++)
+            {
+                var sample = samples[idx];
+
+                if (sample == Path.Combine(prefix, String.Format("{0:d4}.wav", value)) ||
+                    sample == Path.Combine(prefix, String.Format("{0:d4}.wma", value)))
+                {
+                    return idx;
+                }
+            }
+            
+            return -1;
+        }
+
+        private static List<KeyPosition> parseJsonChartData(string filename, List<string> samples)
+        {
+            var sounds = new List<KeyPosition>();
+            var loaded_samples = new List<Dictionary<short, int>>(){
+                new Dictionary<short, int>(),
+                new Dictionary<short, int>()
+            };
+
+            var json = File.ReadAllText(filename);
+            dynamic _chartData = JsonConvert.DeserializeObject(json);
+            dynamic chartData = _chartData["charts"][0];
+
+            var soundFolder = Path.GetDirectoryName(samples[0]);
+
+            foreach (var events in chartData["events"])
+            {
+                foreach (var _ev in events)
+                {
+                    for (int eventidx = 0; eventidx < _ev.Count; eventidx++)
+                    {
+                        var ev = _ev[eventidx];
+                        
+                        int offset = ev["offset"];
+
+                        if ((ev["event"] == "note_p1" || ev["event"] == "note_p2") && offset != 0)
+                        {
+                            var playerId = ev["event"] == "note_p1" ? 0 : 1;
+                            int value = ev["value"];
+                            short param = ev["slot"];
+
+                            if (value != 0)
+                            {
+                                sounds.Add(new KeyPosition(offset + value, loaded_samples[playerId][param], param, playerId));
+
+                                if (options.AssistClap && param == 7)
+                                {
+                                    sounds.Add(new KeyPosition(offset + value, assistClapIdx, -1, 0));
+                                }
+                            }
+
+                            if (options.AssistClap && param == 7)
+                            {
+                                sounds.Add(new KeyPosition(offset, assistClapIdx, -1, 0));
+                            }
+                            
+                            sounds.Add(new KeyPosition(offset, loaded_samples[playerId][param], param, playerId));
+                        }
+
+                        if (ev["event"] == "sample_p1" || ev["event"] == "sample_p2")
+                        {
+                            var playerId = ev["event"] == "sample_p1" ? 0 : 1;
+                            int value = ev["sound_id"];
+                            short param = ev["slot"];
+
+                            loaded_samples[playerId][param] = findSampleById(samples, value, soundFolder);
+
+                            // To simulate the engine loading a new sample before it's played, loop through and update everything
+                            // for that specific key from the current offset onward
+                            for (int i = 0; i < sounds.Count; i++)
+                            {
+                                if (sounds[i].player == playerId && sounds[i].key == param && sounds[i].offset >= offset)
+                                {
+                                    sounds[i].keysoundId = loaded_samples[playerId][param];
+                                }
+                            }
+                        }
+
+                        if (ev["event"] == "auto")
+                        {
+                            int value = ev["sound_id"];
+                            int sample_idx = findSampleById(samples, value, soundFolder);
+
+                            if (sample_idx == 0 && options.NoBgm)
+                            {
+                                continue;
+                            }
+
+                            sounds.Add(new KeyPosition(offset, sample_idx, -1, 0));
+                        }
+                    }
+                }
+            }
+            
+            return sounds;
         }
 
         private static List<KeyPosition> parseChartData(BinaryReader reader, List<string> samples)
@@ -186,7 +293,8 @@ namespace _2dxrender
                 new Dictionary<short, int>()
             };
 
-            while (true) {
+            while (true)
+            {
                 var offset = reader.ReadInt32();
                 var command = reader.ReadByte();
                 var param = reader.ReadByte();
@@ -222,7 +330,7 @@ namespace _2dxrender
                             sounds.Add(new KeyPosition(offset, loaded_samples[playerId][param], param, playerId));
                         }
                         break;
-                        
+
                     case (byte)ChartCommands.LoadSampleP1:
                     case (byte)ChartCommands.LoadSampleP2:
                         {
@@ -266,6 +374,13 @@ namespace _2dxrender
                 goto cleanup;
             }
 
+            if (filename.EndsWith(".json"))
+            {
+                var sounds = parseJsonChartData(filename, samples);
+                mixFinalAudio(outputFilename, sounds, samples);
+                return;
+            }
+
             using (BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open)))
             {
                 reader.BaseStream.Seek(chartId * 8, SeekOrigin.Begin);
@@ -279,7 +394,7 @@ namespace _2dxrender
                 mixFinalAudio(outputFilename, sounds, samples);
             }
 
-cleanup:
+            cleanup:
             foreach (var sampleFilename in samples)
             {
                 File.Delete(sampleFilename);
@@ -290,14 +405,32 @@ cleanup:
         {
             var mixedSamples = new List<OffsetSampleProvider>();
             foreach (var sound in sounds)
-            {
+            {   
+                if (sound.keysoundId == -1)
+                    continue;
+
                 var volSample = new VolumeSampleProvider(new AudioFileReader(samples[sound.keysoundId]));
 
                 if (volSample.WaveFormat.Channels == 1)
                 {
                     volSample = new VolumeSampleProvider(volSample.ToStereo());
                 }
-                
+
+                if (volSample.WaveFormat.SampleRate != 441000)
+                {
+                    // Causes pop sound at end of audio
+                    volSample = new VolumeSampleProvider(
+                        new WaveToSampleProvider(
+                            new MediaFoundationResampler(
+                               new SampleToWaveProvider(volSample),
+                               WaveFormat.CreateIeeeFloatWaveFormat(44100, 2)
+                            ) {
+                                ResamplerQuality = 60
+                            }
+                        )
+                    );
+                }
+
                 if (options.AssistClap && sound.keysoundId == assistClapIdx)
                 {
                     volSample.Volume = options.AssistClapVolume;
@@ -317,7 +450,7 @@ cleanup:
             {
                 mixers.Add(new MixingSampleProvider(mixedSamples.Skip(i).Take(128).ToArray()));
             }
-
+            
             var mixer = new MixingSampleProvider(mixers);
 
             if (options.OutputFormat.ToLower() == "wav")
@@ -357,7 +490,7 @@ cleanup:
         static void Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(opts => options = opts).WithNotParsed(errors => Environment.Exit(1));
-                
+
             var samples = getAudioSamples(options.InputAudio);
             parseChart(options.InputChart, options.ChartId, options.OutputFile, samples);
         }
